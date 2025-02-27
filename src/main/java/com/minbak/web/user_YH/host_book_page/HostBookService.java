d4b5c9c22cb036e32609c77b821dcf4d8e257b30
YH-Ryu <tyuy1018@gmail.com>
2025-02-27 15:51:07 +0900

    호스트 예약관리 페이지 기초기능 구현

--- src/main/java/com/minbak/web/user_YH/host_book_page/HostBookService.java
+++ src/main/java/com/minbak/web/user_YH/host_book_page/HostBookService.java
@@ -1,11 +1,13 @@
 package com.minbak.web.user_YH.host_book_page;
 
 
+import com.minbak.web.user_YH.dto.DetailUserResponse;
 import com.minbak.web.user_YH.payment_page.PaymentBookDto;
 import com.minbak.web.user_YH.payment_page.PaymentRoomDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,5 +18,20 @@ public class HostBookService {
 
     List<HostBooksResponse> findBooksByUserId(int userId){
         return hostBookMapper.findBooksByUserId(userId);
+    }
+
+    DetailUserResponse findUserDetailByUserId(int userId){
+        return hostBookMapper.findUserDetailByUserId(userId);
+    }
+
+    public List<Map<String, String>> getHostingRooms(Integer userId) {
+        return hostBookMapper.fineHostingRoomByUserId(userId);
+    }
+
+    public void approveReservation(Integer bookId) {
+        int updatedRows = hostBookMapper.approveReservation(bookId);
+        if (updatedRows == 0) {
+            throw new IllegalStateException("예약 승인 실패: 해당 bookId가 존재하지 않거나 이미 승인됨.");
+        }
     }
 }
