8ca2808360c76f7a222bad157eeffec4bf95a405
gwangWook218 <akw2080@naver.com>
2025-03-02 17:33:19 +0900

    호스트페이지 수정 중

--- src/main/java/com/minbak/web/host_today/HostTodayDto.java
+++ src/main/java/com/minbak/web/host_today/HostTodayDto.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 
 @Data
 public class HostTodayDto {
-    private String roomName;         // 방 이름
+    private String name;         // 방 이름
     private int userId;              // 사용자 ID
     private LocalDate startDate;          // 예약 시작일
     private LocalDate endDate;            // 예약 종료일
