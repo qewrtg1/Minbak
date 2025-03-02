be865ebf9ab200dd7351fc54ef6844da06d55303
Ryu-HW <tyuy1018@gmail.com>
2025-03-02 14:12:04 +0900

    회원가입 구현

---
+++ src/main/java/com/minbak/web/user_YH/signup/SignupController.java
@@ -0,0 +1,13 @@
+package com.minbak.web.user_YH.signup;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignupController {
+
+    @GetMapping("/signup")
+    public String SignupPage(){
+        return "/user-pages/user-signup";
+    }
+}
